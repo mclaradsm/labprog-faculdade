8772a7ba827ffc3f8fbedc1d2e5751d1e3cfac43
mclarad <mclaradsm@gmail.com>
2026-08-28 23:38:09 -0300

    Atualização da lista

--- lista1/q7.c
+++ lista1/q7.c
@@ -4,6 +4,7 @@ int main(){
     int x;
     puts("Digite um número inteiro e irei elevar ao quadrado: ");
     scanf("%d", &x);
+    
     printf("O quadrado de %d é %d\n", x, x * x);
     return 0;
 }
