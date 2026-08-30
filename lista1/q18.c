93b83dcaf295073836ce4b760b9d65253620103a
mclarad <mclaradsm@gmail.com>
2026-08-29 22:35:48 -0300

    Atualização questão 18

---
+++ lista1/q18.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(){
+    puts("Retângulo: ");
+    printf("********\n");
+    printf("*      *\n");
+    printf("*      *\n");
+    printf("*      *\n");
+    printf("*      *\n");    
+    printf("*      *\n");
+    printf("********\n");
+
+    puts("Elpse: ");
+    printf("   ***\n");
+    printf("  *   *\n");
+    printf(" *     *\n");
+    printf("*       *\n");
+    printf("*       *\n");
+    printf(" *     *\n");
+    printf("  *   *\n");
+    printf("   ***\n");
+
+
+    puts("Seta: ");
+    printf("    *\n");
+    printf("   ***\n");
+    printf("  *****\n");
+    printf("    *\n");
+    printf("    *\n");
+    printf("    *\n");
+    printf("    *\n");
+    printf("    *\n");
+
+
+    puts("Losango: ");
+    printf("    *\n");
+    printf("   * *\n");
+    printf("  *   *\n");
+    printf(" *     *\n");
+    printf("*       *\n");
+    printf(" *     *\n");
+    printf("  *   *\n");
+    printf("   * *\n");
+    printf("    *\n");
+
+
+
+return 0;
+}
