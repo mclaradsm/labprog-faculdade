4da28858b8a466b185567a8c6594e0229e1776f7
mclaradsm <mclaradsm@gmail.com>
2026-09-10 15:24:18 -0300

    att

--- lista2/questao24.c
+++ lista2/questao24.c
@@ -8,12 +8,8 @@ int main() {
         for (cateto2 = 1; cateto2 <= 500; cateto2++) {
             for (hipotenusa = 1; hipotenusa <= 500; hipotenusa++) {
 
-                if (cateto1 * cateto1 +
-                    cateto2 * cateto2 ==
-                    hipotenusa * hipotenusa) {
-
-                    printf("%d %d %d\n",
-                           cateto1, cateto2, hipotenusa);
+                if (cateto1 * cateto1 + cateto2 * cateto2 == hipotenusa * hipotenusa) {
+                    printf("%d %d %d\n", cateto1, cateto2, hipotenusa);
                 }
             }
         }
---
+++ lista2/questao25.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    int i;
+    double pi = 0.0;
+
+    printf("Termos\tValor aproximado de PI\n");
+
+    for (i = 1; i <= 10; i++) {
+        if (i % 2 == 1) {
+            pi = pi + 4.0 / (2 * i - 1);
+        } else {
+            pi = pi - 4.0 / (2 * i - 1);
+        }
+
+        printf("%d\t%.10f\n", i, pi);
+    }
+
+    return 0;
+}
