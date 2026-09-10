1bd1ba030144291660dacea395c3f406caa0fca7
mclaradsm <mclaradsm@gmail.com>
2026-09-10 15:17:30 -0300

    att

---
+++ lista2/questao24.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int cateto1, cateto2, hipotenusa;
+
+    for (cateto1 = 1; cateto1 <= 500; cateto1++) {
+        for (cateto2 = 1; cateto2 <= 500; cateto2++) {
+            for (hipotenusa = 1; hipotenusa <= 500; hipotenusa++) {
+
+                if (cateto1 * cateto1 +
+                    cateto2 * cateto2 ==
+                    hipotenusa * hipotenusa) {
+
+                    printf("%d %d %d\n",
+                           cateto1, cateto2, hipotenusa);
+                }
+            }
+        }
+    }
+
+    return 0;
+}
